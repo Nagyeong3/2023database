4d8fabc0f3139d73376bd1186d038a1c9a73e540
nagyeong kim <skrud0318@naver.com>
2023-05-19 22:56:38 +0900

    slotted_page completed!!

--- main_slotted_page.cpp
+++ main_slotted_page.cpp
@@ -11,46 +11,34 @@ int main(){
 	uint64_t val = 100;
 	uint64_t cnt = 0;
 	
-	for (int i=0;i<5;i++){
-		std::cin>>key>>val;
-		
-		int I=p->insert(key, val);
-		if(I){
-			std::cout<<"\n";
-			p->print();
-			std::cout<<"\n";
-		}else {
-			std::cout<<"실패\n";
+
+	for(i='a'; i<='j'; i++){
+		for(int j=0; j<STRING_LEN-1; j++){
+			key[j] = i;
+		}
+		key[STRING_LEN-1]='\0';
+		cnt++;
+		val*=cnt;
+		p->insert(key, val);
+	}
+p->print();
+	val = 100;
+	cnt = 0;
+	for(i='a'; i<='j'; i++){
+		for(int j=0; j<STRING_LEN-1; j++){
+			key[j] = i;
+		}
+		key[STRING_LEN-1]='\0';
+		cnt++;
+		val*=cnt;
+		if(val== p->find(key)){
+			printf("key :%s founds\n",key);		
+		}
+		else{
+			printf("key :%s Something wrong\n",key);		
+
 		}
 	}
-
-	// for(i='a'; i<='j'; i++){
-	// 	for(int j=0; j<STRING_LEN-1; j++){
-	// 		key[j] = i;
-	// 	}
-	// 	key[STRING_LEN-1]='\0';
-	// 	cnt++;
-	// 	val*=cnt;
-	// 	p->insert(key, val);
-	// }
-//p->print();
-	// val = 100;
-	// cnt = 0;
-	// for(i='a'; i<='j'; i++){
-	// 	for(int j=0; j<STRING_LEN-1; j++){
-	// 		key[j] = i;
-	// 	}
-	// 	key[STRING_LEN-1]='\0';
-	// 	cnt++;
-	// 	val*=cnt;
-	// 	if(val== p->find(key)){
-	// 		printf("key :%s founds\n",key);		
-	// 	}
-	// 	else{
-	// 		printf("key :%s Something wrong\n",key);		
-
-	// 	}
-	// }
 
 	return 0;
 
--- page.cpp
+++ page.cpp
@@ -45,8 +45,31 @@ page *page::get_leftmost_ptr(){
 }
 
 uint64_t page::find(char *key){
-	// Please implement this function in project 1.
 	uint64_t val = 0;
+	int num_data = hdr.get_num_data();
+	void *offset_array=hdr.get_offset_array();
+	uint64_t address;
+	void* new_off;
+	char* stored_key;
+	
+	for(int i =0; i<num_data; i++){
+		new_off = (void*)((uint8_t*)offset_array+i*2);
+		//printf("get new off?? %d\n",get2byte(new_off));
+		address = get2byte(new_off)+2+(uint64_t)this;
+		//printf("주소에 들어간 key: %s\n",(char*)(void*)address);
+		stored_key=(char*)(void*)address;
+		
+		if(strcmp(stored_key,key)==0){
+			address=address+strlen(stored_key)+1;
+			//printf("value address??? : %d\n",(void*)address);
+            //val = get2byte((void*)address);
+			val= get_val((void *)stored_key);
+
+            //printf("Found! val = %lu\n", val);
+            break;
+		}
+	}
+	
 	return val;
 }
 
@@ -62,37 +85,35 @@ bool page::insert(char *key,uint64_t val){
 		return false;
 	}else{
 		//printf("page 끝 지점: %d \n",hdr.get_data_region_off());
-		printf("free space 시작지점: %d \n", data);
+		//printf("free space 시작지점: %d \n", data);
 		// //1) 레코드사이즈 저장
-		printf("key: %s value: %d record size: %d \n",key,val,record_size);
 		//2)데이터가 들어갈 위치계산
 		uint64_t data_dest = hdr.get_data_region_off()-record_size;
-		printf("this 주소: %u\n", (uint64_t)this); 
-		printf("dest addr : %d \n", data_dest);
+		//printf("this 주소: %u\n", (uint64_t)this); 
+		//printf("dest addr : %d \n", data_dest);
 		//레코드 사이즈 저장
 		uint64_t address = (uint64_t)this + data_dest;
 		memcpy((void*)address,&record_size,2);
 
-		printf("data가 들어가는 주소: %u\n", (uint64_t)this + data_dest);
-		printf("this+data_dest 주소에 들어간 Record size: %u\n", get2byte((void*)address));
+		//printf("data가 들어가는 주소: %u\n", (uint64_t)this + data_dest);
 		
 		address = (uint64_t)this + data_dest+2;
 		memcpy((void*)address,key,strlen(key)+1);
-		printf("주소에 들어간 key: %s\n",(char*)(void*)address);
+		//printf("주소에 들어간 key: %s\n",(char*)(void*)address);
 
 		address = (uint64_t)this+data_dest+2+strlen(key)+1;
 		memcpy((void*)address,&val,8);
-		printf("주소에 들어간 val: %u\n", get2byte((void*)address));
+		//printf("%d 주소에 들어간 val: %u\n", (void*)address ,get2byte((void*)address));
 		hdr.set_data_region_off(data_dest);	//데이터 시작 영역 조정(방금 들어온 레코드만큼 앞당기기)
 	
 
 		//offset
-		printf("offset\n");
+		//printf("offset\n");
 		void* new_off = (void*)((uint8_t*)offset_array+num_data*2);
 		for(int i=0; i<num_data; i++){
 			uint64_t curr_off=get2byte(data+i*2);
 			address=(uint64_t)this+curr_off+2;  //offset array에 들어간 키의 주소값
-			printf("curr_off(%d)에 들어간 key = %s\n",curr_off, (char*)(void*)address);
+			//printf("curr_off(%d)에 들어간 key = %s\n",curr_off, (char*)(void*)address);
 
 			  
         	if (strcmp((char*)(void*)address, key) > 0) {
@@ -105,26 +126,22 @@ bool page::insert(char *key,uint64_t val){
 				break;
         	}
 		}
-		//printf("^^^^^^^^offset_array: %d\n",offset_array+num_data*2);
 		
 		if(index==-1){
 			memcpy(new_off,&data_dest,2);
 		}
 		
-		printf("memcpy로 data insert => offset array %u \n",get2byte(new_off));
+		//printf("memcpy로 data insert => offset array %u \n",get2byte(new_off));
 		num_data++;
 		hdr.set_num_data(num_data);
-	
-		
-		//hdr.set_offset_array((void*)((uint64_t)offset_array + 2));
 
-		printf("offset array address(new_off)= %d data address: %d \n", new_off,data);
-		for(int i=0; i<num_data; i++){
-			off= *(uint16_t *)((uint64_t)data+i*2);	
-			printf(" %d |",off);
-		}
-		printf("\n");
-		printf("offset array %u \n -----insert 끝------\n",get2byte(new_off));
+		//printf("offset array address(new_off)= %d data address: %d \n", new_off,data);
+		// for(int i=0; i<num_data; i++){
+		// 	off= *(uint16_t *)((uint64_t)data+i*2);	
+		// 	printf(" %d |",off);
+		// }
+		// printf("\n");
+		// printf("offset array %u \n -----insert 끝------\n",get2byte(new_off));
 
 	}
 		printf("현재 데이터 개수는? : %d\n", num_data);
@@ -148,7 +165,7 @@ bool page::is_full(uint64_t inserted_record_size){
 		header_size= header_size+i*2;
 	}	
 	int freespace = hdr.get_data_region_off()-header_size-hdr.get_num_data()*2-inserted_record_size-sizeof(uint16_t);
-	printf("less than 0 check: %d \n",freespace);
+	//printf("less than 0 check: %d \n",freespace);
 	if(freespace < 0){
 		return true;
 	}else{
